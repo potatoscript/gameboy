3e3873c0fc3e502bc21cb87226833854a3bc3c72
🥔 <>
2025-03-16 19:16:28 +0900

    🎮 Creating & Moving Backgrounds on Game Boy! 🌄🚀

--- code/main.c
+++ code/main.c
@@ -1,43 +1,57 @@
 #include <gb/gb.h>
 #include <stdio.h>
 #include "smileface.c"
+#include "simplebackground.c"
+#include "simplebackgroundmap.c"
 
 void main()
 {
-    UINT8 currentspriteindex = 0;
+    set_bkg_data(0, 7, backgroundtiles);
+    set_bkg_tiles(0, 0, 40, 18, background);
 
-    set_sprite_data(0, 2, TileLabel);
-    set_sprite_tile(0, 0);
-    move_sprite(0, 88, 78);
-    SHOW_SPRITES;
+    SHOW_BKG;
+    DISPLAY_ON;
 
     while (1)
     {
-        switch (joypad())
-        {
-        case J_LEFT:
-            scroll_sprite(0, -5, 0);
-            break;
-        case J_RIGHT:
-            scroll_sprite(0, 5, 0);
-            break;
-        case J_UP:
-            scroll_sprite(0, 0, -5);
-            break;
-        case J_DOWN:
-            scroll_sprite(0, 0, 5);
-            break;
-        }
-
-        if (currentspriteindex == 0)
-        {
-            currentspriteindex = 1;
-        }
-        else
-        {
-            currentspriteindex = 0;
-        }
-        set_sprite_tile(0, currentspriteindex);
+        scroll_bkg(1, 0);
         delay(100);
     }
+
+    // UINT8 currentspriteindex = 0;
+
+    // set_sprite_data(0, 2, TileLabel);
+    // set_sprite_tile(0, 0);
+    // move_sprite(0, 88, 78);
+    // SHOW_SPRITES;
+
+    // while (1)
+    // {
+    //     switch (joypad())
+    //     {
+    //     case J_LEFT:
+    //         scroll_sprite(0, -5, 0);
+    //         break;
+    //     case J_RIGHT:
+    //         scroll_sprite(0, 5, 0);
+    //         break;
+    //     case J_UP:
+    //         scroll_sprite(0, 0, -5);
+    //         break;
+    //     case J_DOWN:
+    //         scroll_sprite(0, 0, 5);
+    //         break;
+    //     }
+
+    //     if (currentspriteindex == 0)
+    //     {
+    //         currentspriteindex = 1;
+    //     }
+    //     else
+    //     {
+    //         currentspriteindex = 0;
+    //     }
+    //     set_sprite_tile(0, currentspriteindex);
+    //     delay(100);
+    // }
 }
